5c65f12f8c7dae53ff54f4a847316f29b483da12
Simon Herensperger <sh@eoss.ch>
2021-09-27 23:18:12 +0200

    initial commit

---
+++ src/config/config.ts
@@ -0,0 +1,12 @@
+const config = {
+    firebase: {
+        apiKey: "AIzaSyDGiYwlAvz73QV5l31Qn5V7vPbisN_EeSw",
+        authDomain: "authtesting-97746.firebaseapp.com",
+        projectId: "authtesting-97746",
+        storageBucket: "authtesting-97746.appspot.com",
+        messagingSenderId: "572695663267",
+        appId: "1:572695663267:web:ba8cd4a2d84f3cbfce6516"
+    }
+}
+
+export default config;
